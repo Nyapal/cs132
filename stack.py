85e08cde00009758a45a6bf39a8163afe761db59
nyapal <nyapal14@gmail.com>
2019-06-20 15:07:57 -0700

    linked stack passing all test

--- stack.py
+++ stack.py
@@ -2,7 +2,6 @@
 
 from linkedlist import LinkedList
 
-
 # Implement LinkedStack below, then change the assignment at the bottom
 # to use this Stack implementation to verify it passes all tests
 class LinkedStack(object):
@@ -21,27 +20,40 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        # TODO: Check if empty
+        # Check if empty
+        return self.list.is_empty()
 
     def length(self):
         """Return the number of items in this stack."""
-        # TODO: Count number of items
+        # Count number of items
+        return self.list.length()
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Push given item
+        # Push given item
+        self.list.append(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        # TODO: Return top item, if any
+        # Return top item, if any
+        if self.is_empty():
+            return None 
+        else:
+            return self.list.tail.data
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return top item, if any
+        # Remove and return top item, if any
+        tail_node = self.list.tail 
+        if self.is_empty():
+            raise ValueError('Stack is empty')
+        else:
+            self.list.delete(self.list.tail.data)
+            return tail_node.data
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
